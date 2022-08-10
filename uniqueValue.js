98e4a5e3347df287bdb09ba9f424f114712deb20
aviash2507 <avinash2632@gmail.com>
2022-08-10 18:00:34 +0530

    done

--- uniqueValue.js
+++ uniqueValue.js
@@ -1,14 +1,19 @@
 function uniqueValue(str) {
-  let word = [];
+  let word = {};
   for (let letter of str) {
-    // word = [...word, letter];
-    if (word.indexOf(letter) !== -1) {
-        return ("this is not an Unique word ");
+    if (word[letter]) {
+      return true;
     }
-    
-    word.push(letter);
+    word[letter] = "exists";
+
+    //     if (word.indexOf(letter) !== -1) {
+    //         return ("this is not an Unique word ");
+    //     }
+
+    //     word.push(letter);
+     }
+     return "This is An Unique word";
   }
-  return "This is An Unique word" ;
-}
+
 console.log(uniqueValue("greatest"));
 console.log(uniqueValue("love"));
